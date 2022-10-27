0a62af8fb12dda437f34935c7487db4730828a0e
Tobias Reich <tobias.reich.ich@gmail.com>
2022-10-27 17:18:41 +0200

    Implementation and tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,82 @@
-export default 'Hello World'
+import util from 'node:util'
+import fs from 'node:fs'
+import chalk from 'chalk'
+import figures from 'figures'
+
+const IDENTIFIER = Symbol('IDENTIFIER')
+
+const defaultOptions = {
+	filter: () => true,
+	indention: 0,
+}
+
+const defaultTypeOptions = {
+	color: 'blue',
+	badge: figures.info,
+	label: 'info',
+	stack: true,
+}
+
+const logMessageToConsole = (prefix, message, substitutions) => {
+	console.log(`${prefix}  ${message}`, ...substitutions)
+}
+
+const logStackToConsole = (stack) => {
+	const [, ...parsedStack] = stack.split('\n')
+	const printableStack = parsedStack.join('\n')
+
+	console.log(chalk.gray(printableStack))
+}
+
+const logMessageToFile = (file, message, substitutions) => {
+	fs.appendFileSync(file, util.format(message, ...substitutions) + '\n')
+}
+
+export const createLogger = (types, options) => {
+	const methods = Object.entries(types).reduce((methods, [typeName, typeOptions]) => {
+		const isNestedInstance = typeOptions[IDENTIFIER] === true
+		if (isNestedInstance === true) {
+			return {
+				...methods,
+				[typeName]: typeOptions,
+			}
+		}
+
+		const { filter, indention } = {
+			...defaultOptions,
+			...options,
+		}
+
+		const { color, badge, label, stack, file } = {
+			...defaultTypeOptions,
+			...typeOptions,
+		}
+
+		const method = (message, ...substitutions) => {
+			const included = filter(typeName) === true
+			const prefix = chalk[color](badge + '  ' + label + ' '.repeat(Math.max(0, indention - label.length)))
+
+			// Log message to console
+			if (included === true) logMessageToConsole(prefix, message, substitutions)
+
+			// Check if the message is an error with a stack
+			const hasErrorStack = message instanceof Error && message.stack != null
+			if (hasErrorStack === true && stack === true && included === true) logStackToConsole(message.stack)
+
+			// Log message to file
+			if (file != null) logMessageToFile(file, message, substitutions)
+		}
+
+		return {
+			...methods,
+			[typeName]: method,
+		}
+	}, {})
+
+	return {
+		// Unique key used to identity logger instances
+		[IDENTIFIER]: true,
+		// Type methods
+		...methods,
+	}
+}
--- test/index.js
+++ test/index.js
@@ -1,5 +1,31 @@
 import test from 'ava'
 
-test.serial('passes', (t) => {
-	t.pass()
+import { createLogger } from '../src/index.js'
+
+test.serial('returns logger', (t) => {
+	const instance = createLogger({
+		info: {
+			color: 'blue',
+			badge: 'i',
+			label: 'info',
+			stack: true,
+		},
+	})
+
+	t.is(typeof instance.info, 'function')
+})
+
+test.serial('returns nested logger', (t) => {
+	const instance = createLogger({
+		instance: createLogger({
+			info: {
+				color: 'blue',
+				badge: 'i',
+				label: 'info',
+				stack: true,
+			},
+		}),
+	})
+
+	t.is(typeof instance.instance.info, 'function')
 })
