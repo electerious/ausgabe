59cb8f405ea7f55747eeae854bfb8796237d0e1a
Tobias Reich <tobias@electerious.com>
2025-09-10 17:03:23 +0200

    Use withResolvers in tests

--- test/index.js
+++ test/index.js
@@ -7,147 +7,147 @@ import { format } from 'node:util'
 import { createLogger } from '../src/index.js'
 
 test('logs badge', () => {
-  return new Promise((resolve) => {
-    const badge = uuid()
+  const { resolve } = Promise.withResolvers()
 
-    const stream = new Writable({
-      write: (chunk, encoding, callback) => {
-        assert.ok(chunk.toString().includes(badge))
-        callback()
-        resolve()
-      },
-    })
+  const badge = uuid()
 
-    const instance = createLogger({
-      info: {
-        badge,
-        streams: [stream],
-      },
-    })
-
-    instance.info()
+  const stream = new Writable({
+    write: (chunk, encoding, callback) => {
+      assert.ok(chunk.toString().includes(badge))
+      callback()
+      resolve()
+    },
   })
+
+  const instance = createLogger({
+    info: {
+      badge,
+      streams: [stream],
+    },
+  })
+
+  instance.info()
 })
 
 test('logs label', () => {
-  return new Promise((resolve) => {
-    const label = uuid()
+  const { resolve } = Promise.withResolvers()
 
-    const stream = new Writable({
-      write: (chunk, encoding, callback) => {
-        assert.ok(chunk.toString().includes(label))
-        callback()
-        resolve()
-      },
-    })
+  const label = uuid()
 
-    const instance = createLogger({
-      info: {
-        label,
-        streams: [stream],
-      },
-    })
-
-    instance.info()
+  const stream = new Writable({
+    write: (chunk, encoding, callback) => {
+      assert.ok(chunk.toString().includes(label))
+      callback()
+      resolve()
+    },
   })
+
+  const instance = createLogger({
+    info: {
+      label,
+      streams: [stream],
+    },
+  })
+
+  instance.info()
 })
 
 test('logs string message', () => {
-  return new Promise((resolve) => {
-    const message = uuid()
+  const { resolve } = Promise.withResolvers()
 
-    const stream = new Writable({
-      write: (chunk, encoding, callback) => {
-        assert.ok(chunk.toString().includes(message))
-        callback()
-        resolve()
-      },
-    })
+  const message = uuid()
 
-    const instance = createLogger({
-      info: {
-        streams: [stream],
-      },
-    })
-
-    instance.info(message)
+  const stream = new Writable({
+    write: (chunk, encoding, callback) => {
+      assert.ok(chunk.toString().includes(message))
+      callback()
+      resolve()
+    },
   })
+
+  const instance = createLogger({
+    info: {
+      streams: [stream],
+    },
+  })
+
+  instance.info(message)
 })
 
 test('logs object message', () => {
-  return new Promise((resolve) => {
-    const message = { test: uuid() }
+  const { resolve } = Promise.withResolvers()
 
-    const stream = new Writable({
-      write: (chunk, encoding, callback) => {
-        assert.ok(chunk.toString().includes(format(message)))
-        callback()
-        resolve()
-      },
-    })
+  const message = { test: uuid() }
 
-    const instance = createLogger({
-      info: {
-        streams: [stream],
-      },
-    })
-
-    instance.info(message)
+  const stream = new Writable({
+    write: (chunk, encoding, callback) => {
+      assert.ok(chunk.toString().includes(format(message)))
+      callback()
+      resolve()
+    },
   })
+
+  const instance = createLogger({
+    info: {
+      streams: [stream],
+    },
+  })
+
+  instance.info(message)
 })
 
 test('logs error message', () => {
-  return new Promise((resolve) => {
-    const message = new Error(uuid())
+  const { resolve } = Promise.withResolvers()
 
-    const stream = new Writable({
-      write: (chunk, encoding, callback) => {
-        assert.ok(chunk.toString().includes(message.message))
-        callback()
-        resolve()
-      },
-    })
+  const message = new Error(uuid())
 
-    const instance = createLogger({
-      info: {
-        stack: false,
-        streams: [stream],
-      },
-    })
-
-    instance.info(message)
+  const stream = new Writable({
+    write: (chunk, encoding, callback) => {
+      assert.ok(chunk.toString().includes(message.message))
+      callback()
+      resolve()
+    },
   })
+
+  const instance = createLogger({
+    info: {
+      stack: false,
+      streams: [stream],
+    },
+  })
+
+  instance.info(message)
 })
 
 test('logs error stack', () => {
-  return new Promise((resolve) => {
-    let count = 0
+  const { resolve } = Promise.withResolvers()
 
-    const message = new Error(uuid())
+  let count = 0
 
-    const stream = new Writable({
-      write: (chunk, encoding, callback) => {
-        count += 1
-        callback()
+  const message = new Error(uuid())
 
-        // Give it time to accumulate more writes for stack
-        setTimeout(() => {
-          // The test only includes one log call which means we can assume that
-          // multiple write stream calls mean that the stack has been logged.
-          assert.ok(count > 1)
-          resolve()
-        }, 10)
-      },
-    })
-
-    const instance = createLogger({
-      info: {
-        streams: [stream],
-      },
-    })
-
-    instance.info(message)
+  const stream = new Writable({
+    write: (chunk, encoding, callback) => {
+      count += 1
+      callback()
+    },
   })
+
+  const instance = createLogger({
+    info: {
+      streams: [stream],
+    },
+  })
+
+  instance.info(message)
+
+  // Give it time to accumulate more writes for stack
+  setTimeout(() => {
+    // The test only includes one log call which means we can assume that
+    // multiple write stream calls mean that the stack has been logged.
+    assert.ok(count > 1)
+    resolve()
+  }, 10)
 })
 
 test('returns nested logger', () => {
